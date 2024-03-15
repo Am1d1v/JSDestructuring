3299defeb9113a10be8fd3220c6b4b2d1ae4e493
Am1d1v <dimadiman1adima@gmail.com>
2024-03-15 17:38:59 +0300

    Array Destructuring

--- Destructuring.js
+++ Destructuring.js
@@ -1,3 +1,31 @@
 
 
 
+    // Array Destructuring
+
+// Initial Restaurant Data
+const restaurantData = {
+    name: 'Banzai',
+    location: '108 Wood Road',
+    categories: ['Japanese', 'Sushi', 'Vegeterian'],
+    mainMenu: ['Sushi', 'Ramen'],
+    appetizers: ['Sushi rice', 'Tempura', 'Salad'],
+    orderFood: function(appetizersIndex, mainMenuIndex){
+        return [this.appetizers[appetizersIndex], this.mainMenu[mainMenuIndex]]
+    }
+}
+
+/* const arr = [1, 2, 3];
+// const value1 = arr[0];
+// const value2 = arr[1];
+// const value3 = arr[2];
+// console.log(value1);
+
+const [value1, value2] = arr;
+console.log(value1, value2); */
+
+// const [categoryOne, ,categoryThree] = restaurantData.categories;
+// console.log(categoryOne, categoryThree);
+
+const [appetizer, mainMenu] = restaurantData.orderFood(1, 1);
+console.log(appetizer, mainMenu);
